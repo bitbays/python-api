54f633468fa3c1dc6a264cb0969fdcd03e7f9e3d
ichuan <iyanchuan@gmail.com>
2014-08-12 19:42:00 +0800

    versionning

--- api.py
+++ api.py
@@ -11,13 +11,14 @@ class API(object):
   public_methods = ['ticker', 'trades', 'depth', 'test']
   private_methods = ['info', 'orders', 'transactions', 'trade', 'cancel']
   timeout = 30
-  api_base = 'https://bitbays.com/api'
+  api_base = 'https://bitbays.com/api/'
 
-  def __init__(self, key, secret, wait_for_nonce=False):
+  def __init__(self, key, secret, wait_for_nonce=False, version='v1'):
     self._key = key
     self._secret = secret
     self._wait_for_nonce = wait_for_nonce
     self._methods = self.public_methods + self.private_methods
+    self._api_base = self.api_base + version
 
   def _nonce(self):
     if self._wait_for_nonce:
@@ -32,12 +33,12 @@ class API(object):
       params['nonce'] = self._nonce()
     params = urllib.urlencode(params)
     if private:
-      req = urllib2.Request(self.api_base + uri, params, {
+      req = urllib2.Request(self._api_base + uri, params, {
         'Key': self._key,
         'Sign': self._sign(params),
       })
     else:
-      req = urllib2.Request('%s%s?%s' % (self.api_base, uri, params))
+      req = urllib2.Request('%s%s?%s' % (self._api_base, uri, params))
     return json.load(urllib2.urlopen(req, timeout=self.timeout))
 
   def __getattr__(self, name):
